e26d79ce4a27bc9c84f6b5b1becc5fbf7b53908a
nachogamo <86362536+nachogamo@users.noreply.github.com>
2025-07-29 18:30:26 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('BeatFlow PWA activo');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';  // Si tienes archivos CSS
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Este es el contenedor donde React se renderiza
+);
