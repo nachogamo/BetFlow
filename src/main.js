349d922894470cdaa34ac98870ba4bb2cffd50e2
Ignacio <ignacio.gallardo.monsal@hotmail.com>
2025-07-29 18:49:15 -0400

    Corregir rutas y estructura de archivos

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';  // Si tienes archivos CSS
+import App from './App';  // Asegúrate de que App.js está en la carpeta src
+import './index.css';  // Si tienes un archivo CSS
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root') // Este es el contenedor donde React se renderiza
+  document.getElementById('root')  // Este es el div donde React va a renderizar la app
 );
